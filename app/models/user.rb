cfbdc9b93e3cf3a59af7aa0aa79c56ff6ddfdce4
mas-i <fd27763s@gmail.com>
2020-09-28 16:23:54 +0900

    validates_setting

---
+++ app/models/user.rb
@@ -0,0 +1,15 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  validates :nick_name, presence: true, uniqueness: true
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, uniqueness: true, length: { maximum: 255 }, format: {with: VALID_EMAIL_REGEX, allow_blank: true}
+  validates :password, presence: true, length: { minimum: 7, maximum: 128}
+
+  has_many :fruits
+  has_many :favorites
+  has_many :reviews
+end
