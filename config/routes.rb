531ecf8b79a8988a5244d439997155be366c0579
mas-i <fd27763s@gmail.com>
2020-10-09 21:42:45 +0900

    variety_controller_create

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,5 @@ Rails.application.routes.draw do
   root "fruits#index"
 
   resources :fruits
+  resources :variety
 end
---
+++ spec/requests/varieties_request_spec.rb
@@ -0,0 +1,5 @@
+require 'rails_helper'
+
+RSpec.describe "Varieties", type: :request do
+
+end
